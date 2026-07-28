4d9ce79d4317824d46ee0675895f2c382e0342dd
KNTRI <128505388+KNTRI@users.noreply.github.com>
2026-07-28 18:40:51 +0900

    Add minimal signal sensing (#4)
    Add minimal signal sensing

--- tests/Omokane.Core.Misogi/Program.fs
+++ tests/Omokane.Core.Misogi/Program.fs
@@ -8,6 +8,7 @@ let 全テスト =
         "思兼神Core 禊Test"
         [
             ObservationGenerationTests.全テスト
+            SensingTests.全テスト
             CausalOperationExecutionTests.全テスト
             CausalOperationBatchExecutionTests.全テスト
             CausalLedgerTests.全テスト
