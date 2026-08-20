dea86612d1dcff219bf53c2493c0c75b7c0df943
kntri <reettywe2@outlook.jp>
2026-08-20 18:55:43 +0900

    Add voluntary locomotion intent gate

--- tests/Omokane.Core.Misogi/Program.fs
+++ tests/Omokane.Core.Misogi/Program.fs
@@ -27,6 +27,7 @@ let 全テスト =
             EntityReactionStateCausalTests.全テスト
             IntegratedEntityReactionHistoryTests.全テスト
             VoluntaryLocomotionControlTests.全テスト
+            VoluntaryLocomotionIntentGateTests.全テスト
         ]
 
 [<EntryPoint>]
